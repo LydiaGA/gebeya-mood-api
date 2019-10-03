8d9afa3318345566c4702a673dec2e9ec340b80e
Lydia Gashawtena <lydiagashawtena@gmail.com>
2019-10-03 20:10:32 +0300

    Set up Project

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+mongoose.connect("mongodb://localhost/gebeya_mood", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('error', function errHandler(){
+    console.log("Connection Error");  
+});
+
+mongoose.connection.once('open', function errHandler(){
+    console.log("Connected to Database");  
+});
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
