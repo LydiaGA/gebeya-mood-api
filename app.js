f1f9d1ee7de0bf7e8bcea727793d30da9f2e5969
Lydia <lydiagashawtena@gmail.com>
2019-10-31 11:57:08 +0300

    Fix mongo url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const router = require('./routes');
 
-mongoose.connect(process.env.MONGODB_URI, 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://lydia:mood_pass2@ds339348.mlab.com:39348/db_gebeya_mood", 
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 // mongoose.connect(process.env.MONGODB_URI || config.MONGODB_URL, 
@@ -18,7 +18,7 @@ mongoose.connection.on('connected', function mongoListener(err) {
 mongoose.connection.on('error', function mongoErrorListener(err) {
   console.log('Connecting to MongoDB failed!');
 
-  mongoose.connect(process.env.MONGODB_URI,
+  mongoose.connect(process.env.MONGODB_URI || "mongodb://lydia:mood_pass2@ds339348.mlab.com:39348/db_gebeya_mood",
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 // mongoose.connect(process.env.MONGODB_URI || config.MONGODB_URL, 
