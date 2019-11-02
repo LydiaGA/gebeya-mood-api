027b63967b633bbf886c7b951f70d6a8981cde41
Lydia <lydiagashawtena@gmail.com>
2019-11-02 20:22:38 +0300

    Fix index route

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@ const reasonsRouter = require('./reasons');
 const teamMoodsRouter = require('./team_moods');
 
 module.exports = function iniRouter(app) {
-	app.use('/', function(req, res, next){
-		res.status(200);
-		res.json("Link to API Documentation : https://documenter.getpostman.com/view/6477566/SVtZvRrJ?version=latest");
-	});
 	app.use('/users', usersRouter);
 	app.use('/moods', moodsRouter);
 	app.use('/reasons', reasonsRouter);
 	app.use('/team-moods', teamMoodsRouter);
+	app.use('/', function(req, res, next){
+		res.status(200);
+		res.json("Link to API Documentation : https://documenter.getpostman.com/view/6477566/SVtZvRrJ?version=latest");
+	});
 };
