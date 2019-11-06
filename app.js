59c052c03d16ba59491a46b5c1e009ae0d9a1af3
Elias Andualem <eliasandualem8@gmail.com>
2019-11-06 13:00:45 +0300

    changed mongodb url

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+const config = require('./config');
 const router = require('./routes');
 
-mongoose.connect(process.env.MONGODB_URI,
-{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL || "mongodb://lydia:mood_pass2@ds339348.mlab.com:39348/db_gebeya_mood",
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+// mongoose.connect(config.MONGODB_URL, 
+//   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 mongoose.connection.on('connected', function mongoListener(err) {
   console.log('Mongodb connected successfully');
@@ -15,8 +18,11 @@ mongoose.connection.on('connected', function mongoListener(err) {
 mongoose.connection.on('error', function mongoErrorListener(err) {
   console.log('Connecting to MongoDB failed!');
 
-  mongoose.connect(process.env.MONGODB_URI, 
+  mongoose.connect(process.env.MONGODB_URL || "mongodb://lydia:mood_pass2@ds339348.mlab.com:39348/db_gebeya_mood",
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+// mongoose.connect(config.MONGODB_URL, 
+//   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 });
 
 
